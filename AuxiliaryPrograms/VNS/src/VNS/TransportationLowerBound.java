f986bdd6e73029314317a16e91b57bb640c30c9a
bilal.acar.a@gmail.com <bilal.acar.a@gmail.com@0722a8b5-72ae-73a5-d115-9192da592fae>
2012-03-01 00:14:44 +0000

    Random node positioning and GAMS model generation, and drawing a given cluster structure is completed.

--- AuxiliaryPrograms/VNS/src/VNS/TransportationLowerBound.java
+++ AuxiliaryPrograms/VNS/src/VNS/TransportationLowerBound.java
@@ -5,85 +5,65 @@ import ilog.concert.*;
 
 public class TransportationLowerBound {
     
-    
-    static void transport(){
-        try {
-         // Create the modeler/solver object
-         IloCplex cplex = new IloCplex();
+	public static double solve(boolean isGAP) {
+		double objVal = 0;
+		try {
+			IloCplex cplex = new IloCplex();
 
-         IloNumVar[][] var = new IloNumVar[1][];
-         IloRange[][]  rng = new IloRange[1][];
+			int nbMedians = VNSMain.numberOfClusters;
+			int nbNodes = VNSMain.numberOfNodes;
+			double capacity = VNSMain.capacity;
 
-         // Evaluate command line option and call appropriate populate method.
-         // The created ranges and variables are returned as element 0 of arrays
-         // var and rng.
-         populateByRow(cplex, var, rng);
-         
-         // write model to file
-         cplex.exportModel("lpex1.lp");
+			IloNumVar[][] x = new IloNumVar[nbNodes][];
 
-         // solve the model and display the solution if one was found
-         if ( cplex.solve() ) {
-            double[] x     = cplex.getValues(var[0]);
-            double[] dj    = cplex.getReducedCosts(var[0]);
-            double[] pi    = cplex.getDuals(rng[0]);
-            double[] slack = cplex.getSlacks(rng[0]);
+			for (int i = 0; i < nbNodes; i++) {
+				if(isGAP)
+					x[i] = cplex.numVarArray(nbMedians, 0., 1.);
+				else
+					x[i] = cplex.boolVarArray(nbMedians);
+			} 
 
-            cplex.output().println("Solution status = " + cplex.getStatus());
-            cplex.output().println("Solution value  = " + cplex.getObjValue());
+			for (int i = 0; i < nbNodes; i++)       // assure that every node is assigned to a cluster
+				cplex.addEq(cplex.sum(x[i]), 1);
 
-            int nvars = x.length;
-            for (int j = 0; j < nvars; ++j) {
-               cplex.output().println("Variable " + j +
-                                      ": Value = " + x[j] +
-                                      " Reduced cost = " + dj[j]);
-            }
+			for (int j = 0; j < nbMedians; j++) {     // Assure that none of the clusters exceeds its node limit
+				IloLinearNumExpr v = cplex.linearNumExpr(); 
+				for(int i = 0; i < nbNodes; i++)
+					v.addTerm(1., x[i][j]);
+				cplex.addLe(v, capacity);
+			}      
 
-            int ncons = slack.length;
-            for (int i = 0; i < ncons; ++i) {
-               cplex.output().println("Constraint " + i +
-                                     ": Slack = " + slack[i] +
-                                     " Pi = " + pi[i]);
-            }
-         }
-         cplex.end();
-      }
-      catch (IloException e) {
-         System.err.println("Concert exception '" + e + "' caught");
-      }
-    }
-    
-    // The following methods all populate the problem with data for the following
-    // linear program:
-    //
-    //    Maximize
-    //     x1 + 2 x2 + 3 x3
-    //    Subject To
-    //     - x1 + x2 + x3 <= 20
-    //     x1 - 3 x2 + x3 <= 30
-    //    Bounds
-    //     0 <= x1 <= 40
-    //    End
-    //
-    // using the IloMPModeler API
-    static void populateByRow(IloMPModeler model,
-                             IloNumVar[][] var,
-                             IloRange[][] rng) throws IloException {
-      double[]    lb      = {0.0, 0.0, 0.0};
-      double[]    ub      = {40.0, Double.MAX_VALUE, Double.MAX_VALUE};
-      String[]    varname = {"x1", "x2", "x3"};
-      IloNumVar[] x       = model.numVarArray(3, lb, ub, varname);
-      var[0] = x;
+			
+			
+			
+			IloLinearNumExpr expr = cplex.linearNumExpr();	//Construct objective function
+			for (int i = 0; i < nbNodes; ++i) {
+				for (int j = 0; j < nbMedians; ++j) {
+					expr.addTerm(x[i][j], VNSMain.clusterCenters.get(j).distance(VNSMain.nodes.get(i)));
+				}
+			}
 
-      double[] objvals = {1.0, 2.0, 3.0};
-      model.addMaximize(model.scalProd(x, objvals));
+			cplex.addMinimize(expr);
 
-      rng[0] = new IloRange[2];
-      rng[0][0] = model.addLe(model.sum(model.prod(-1.0, x[0]),
-                                        model.prod( 1.0, x[1]),
-                                        model.prod( 1.0, x[2])), 20.0, "c1");
-      rng[0][1] = model.addLe(model.sum(model.prod( 1.0, x[0]),
-                                        model.prod(-3.0, x[1]),
-                                        model.prod( 1.0, x[2])), 30.0, "c2");
+			//cplex.exportModel("TransportModel.lp");
+			if ( cplex.solve() ) {
+//				System.out.println(" - Solution: "); 
+				for (int i = 0; i < nbNodes; ++i) {
+					System.out.print("   " + i + ": ");
+					for (int j = 0; j < nbMedians; ++j)
+						if(cplex.getValue(x[i][j]) != 0.0)
+							System.out.print("" + cplex.getValue(x[i][j]) + "\t");
+					System.out.println();
+				}
+//				System.out.println("   Cost = " + cplex.getObjValue()); 
+				objVal = cplex.getObjValue();
+			}
+			
+			cplex.end();
+		}
+		catch (IloException exc) {
+			System.out.println(exc);
+		}
+		return objVal;
    }
 }
--- AuxiliaryPrograms/VNS/src/VNS/VNSMain.java
+++ AuxiliaryPrograms/VNS/src/VNS/VNSMain.java
@@ -1,19 +1,73 @@
 package VNS;
 
+import cern.jet.random.Uniform;
+import cern.jet.random.engine.MersenneTwister;
 import java.awt.geom.Point2D;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
 
 public class VNSMain {
 	
     static int numberOfNodes = 1500;
     static int numberOfClusters = 30;
+	static int capacity = 75;
     static ArrayList<Point2D.Double> nodes = new ArrayList<Point2D.Double>();
     static ArrayList<Point2D.Double> clusterCenters = new ArrayList<Point2D.Double>();
-    
+    static Uniform uniform;
 	public static void main(String []args){
+		uniform = new Uniform(new MersenneTwister(new Date()));
 		
+		parsePositions("untitled8_positions.txt");
+		System.out.println(TransportationLowerBound.solve(false));
+		//System.out.println(TransportationLowerBound.solve(true));
+		System.out.println(NativeLowerBound.findObjective());
 	}
 
+	static void randomlyPositionNodes()
+	{
+		for(int i=0;i<numberOfNodes;i++){
+			nodes.add(new Point2D.Double(uniform.nextDoubleFromTo(-1500, 1500), uniform.nextDoubleFromTo(-1500, 1500)));
+		}
+		for(int i=0;i<numberOfClusters;){
+			int ind = uniform.nextIntFromTo(0, numberOfNodes-1);
+			if(clusterCenters.contains(nodes.get(ind)))
+				continue;
+			clusterCenters.add(nodes.get(ind));
+			i++;
+		}
+	}
+	
+	static void parsePositions(String inFile)
+	{
+		Scanner input = null;
+		if(!nodes.isEmpty())
+			nodes.clear();
+		try {
+			input = new Scanner(new FileInputStream(inFile));
+		} catch (FileNotFoundException ex) {
+			
+		}
+		
+		numberOfNodes = input.nextInt();
+		numberOfClusters = input.nextInt();
+		Double.parseDouble(input.next());
+		for(int i=0;i<numberOfNodes;i++){
+			nodes.add(new Point2D.Double(Double.parseDouble(input.next()), Double.parseDouble(input.next())));
+		}
+		input.close();
+		
+		for(int i=0;i<numberOfClusters;){
+			int ind = uniform.nextIntFromTo(0, numberOfNodes-1);
+			if(clusterCenters.contains(nodes.get(ind)))
+				continue;
+			clusterCenters.add(nodes.get(ind));
+			i++;
+		}
+	}
+	
     public VNSMain() {
     }
 	
