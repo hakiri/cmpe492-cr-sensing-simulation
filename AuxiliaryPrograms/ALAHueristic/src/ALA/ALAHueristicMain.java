9f0adb84e7a6903dc73596226a7a8481c318ed2b
bilal.acar.a@gmail.com <bilal.acar.a@gmail.com@0722a8b5-72ae-73a5-d115-9192da592fae>
2012-03-12 15:27:09 +0000

    Empty cluster generation problem is resolved.

--- AuxiliaryPrograms/ALAHueristic/src/ALA/ALAHueristicMain.java
+++ AuxiliaryPrograms/ALAHueristic/src/ALA/ALAHueristicMain.java
@@ -5,9 +5,12 @@ import cern.jet.random.Uniform;
 import cern.jet.random.engine.MersenneTwister;
 import cern.jet.random.engine.RandomEngine;
 import java.awt.geom.Point2D;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -24,14 +27,24 @@ public class ALAHueristicMain {
 	static ArrayList<ArrayList<Integer>> yij = new ArrayList<>();
 	
 	public static void main(String[] args) {
+		String fileName = "";
+		boolean random = false;
 		if(args.length > 0){
-			numberOfNodes = Integer.parseInt(args[0]);
-			numberOfClusters = Integer.parseInt(args[1]);
-			isSimulationOn = Integer.parseInt(args[2]) != 0;
+			random = Integer.parseInt(args[0]) == 0;
+			if(random)
+				numberOfNodes = Integer.parseInt(args[1]);
+			else
+				fileName = args[1];
+			numberOfClusters = Integer.parseInt(args[2]);
+			isSimulationOn = Integer.parseInt(args[3]) != 0;
+
 		}
 		uniform = new Uniform(randEngine);
 		long begin = System.currentTimeMillis();
-		initializeNodePositions();
+		if(random)
+			initializeNodePositions();
+		else
+			parsePositions(fileName);
 		double prevObjVal = 1000000000;
 		double  newObjVal =  999999999;
 		DrawCell cell = new DrawCell((int)radius, numberOfNodes, numberOfClusters, 40, yij);
@@ -53,6 +66,10 @@ public class ALAHueristicMain {
 				Logger.getLogger(ALAHueristicMain.class.getName()).log(Level.SEVERE, null, ex);
 			}
 		}
+		clusterCenters.clear();
+		for(int i = 0;i < numberOfClusters ; i++)
+			clusterCenters.add(solveOneMedian(i));
+		newObjVal = objectiveValue();
 		begin = System.currentTimeMillis() - begin;
 		System.out.println("Objective value of the problem: "+newObjVal);
 		System.out.println("Number of iterations: "+ite);
@@ -129,11 +146,30 @@ public class ALAHueristicMain {
 	{
 		double x1Numerator=0, x2Numerator=0;
 		int numberOfNodesInCluster = yij.get(cluster).size();
+		if(numberOfNodesInCluster == 0)
+			return new Point2D.Double(0, 0);
 		for(int i=0;i<numberOfNodesInCluster;i++){
 			x1Numerator += nodes.get(yij.get(cluster).get(i)).x;
 			x2Numerator += nodes.get(yij.get(cluster).get(i)).y;
 		}
 		return new Point2D.Double(x1Numerator/numberOfNodesInCluster, x2Numerator/numberOfNodesInCluster);
+	}
+	
+	static Point2D.Double solveOneMedian(int cluster)
+	{
+		ArrayList<Double> distances = new ArrayList<>();
+		int numberOfNodesInCluster = yij.get(cluster).size();
+		for(int i=0;i<numberOfNodesInCluster;i++){
+			double dist = 0;
+			Point2D.Double n = nodes.get(yij.get(cluster).get(i));
+			for(int j=0;j<numberOfNodesInCluster;j++){
+				dist += n.distance(nodes.get(yij.get(cluster).get(j)));
+			}
+			distances.add(dist);
+		}
+		double min = Collections.min(distances);
+		int node = distances.indexOf(min);
+		return nodes.get(yij.get(cluster).get(node));
 	}
 	
 	/**
@@ -145,29 +181,84 @@ public class ALAHueristicMain {
 			yij.add(new ArrayList<Integer>());
 		
 		for(int i=0;i<numberOfNodes;i++){
-			nodes.add(deployNode(0, 360, 0, radius));
-			int cluster = uniform.nextIntFromTo(0, numberOfClusters - 1);
-			yij.get(cluster).add(i);
+			nodes.add(deployNode(radius));
+			if(i < numberOfClusters)
+				yij.get(i).add(i);
+			else if(nodes.get(i).x >= 0 && nodes.get(i).y >= 0){
+				int cluster = uniform.nextIntFromTo(0, numberOfClusters/4);
+				yij.get(cluster).add(i);
+			}
+			else if(nodes.get(i).x <= 0 && nodes.get(i).y >= 0){
+				int cluster = uniform.nextIntFromTo(numberOfClusters/4 + 1, numberOfClusters/2);
+				yij.get(cluster).add(i);
+			}
+			else if(nodes.get(i).x <= 0 && nodes.get(i).y <= 0){
+				int cluster = uniform.nextIntFromTo(numberOfClusters/2, (3*numberOfClusters)/4);
+				yij.get(cluster).add(i);
+			}
+			else if(nodes.get(i).x >= 0 && nodes.get(i).y <= 0){
+				int cluster = uniform.nextIntFromTo((3*numberOfClusters)/4, numberOfClusters - 1);
+				yij.get(cluster).add(i);
+			}
 		}
+	}
+	
+	static void parsePositions(String inFile)
+	{
+		for(int i=0;i<numberOfClusters;i++)
+			yij.add(new ArrayList<Integer>());
+		
+		Scanner input = null;
+		if(!nodes.isEmpty())
+			nodes.clear();
+		try {
+			input = new Scanner(new FileInputStream(inFile));
+		} catch (FileNotFoundException ex) {
+			
+		}
+		
+		numberOfNodes = input.nextInt();
+		numberOfClusters = input.nextInt();
+		Double.parseDouble(input.next());
+		for(int i=0;i<numberOfNodes;i++){
+			nodes.add(new Point2D.Double(Double.parseDouble(input.next()), Double.parseDouble(input.next())));
+			if(i < numberOfClusters)
+				yij.get(i).add(i);
+			else if(nodes.get(i).x >= 0 && nodes.get(i).y >= 0){
+				int cluster = uniform.nextIntFromTo(0, numberOfClusters/4);
+				yij.get(cluster).add(i);
+			}
+			else if(nodes.get(i).x <= 0 && nodes.get(i).y >= 0){
+				int cluster = uniform.nextIntFromTo(numberOfClusters/4 + 1, numberOfClusters/2);
+				yij.get(cluster).add(i);
+			}
+			else if(nodes.get(i).x <= 0 && nodes.get(i).y <= 0){
+				int cluster = uniform.nextIntFromTo(numberOfClusters/2, (3*numberOfClusters)/4);
+				yij.get(cluster).add(i);
+			}
+			else if(nodes.get(i).x >= 0 && nodes.get(i).y <= 0){
+				int cluster = uniform.nextIntFromTo((3*numberOfClusters)/4, numberOfClusters - 1);
+				yij.get(cluster).add(i);
+			}
+		}
+		input.close();
 	}
 	
 	/**
      * Finds a random position in the cell with respect to given angles and distances.
      * This method can be used as creating random position in the cell or 
      * can be used as creating a random position in a specified zone.
-     * @param angle_small   The minimum angle that random point can have.
-     * @param angle_big     The maximum angle that random point can have.
-     * @param distance_small    The minimum distance from center of the cell to that random point.
-     * @param distance_big      The maximum distance from center of the cell to that random point.
+     * @param radius      The maximum distance from center of the cell to that random point.
      * @return Position of the node.
      */
-    private static Point2D.Double deployNode(double angle_small,double angle_big,double distance_small,double distance_big){
+    private static Point2D.Double deployNode(double radius){
         Point2D.Double position_of_node = new Point2D.Double(0, 0); //initializes the position of the node
-        
-        double random_distance = uniform.nextDoubleFromTo(distance_small,distance_big); 
-        double random_angle = uniform.nextDoubleFromTo( angle_small, angle_big);
-        position_of_node.x = random_distance * Math.cos((random_angle/180)*Math.PI);  //finding x and y axis by using
-        position_of_node.y = random_distance * Math.sin((random_angle/180)*Math.PI);  //angle and distance to center.
+        while(true){
+			position_of_node.x = uniform.nextDoubleFromTo(-radius, radius);
+			position_of_node.y = uniform.nextDoubleFromTo(-radius, radius);
+			if(position_of_node.distance(new Point2D.Double(0, 0))<radius)
+				break;
+		}
         
         return position_of_node;
     }
